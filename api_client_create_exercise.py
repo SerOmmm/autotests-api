972beec236547ecf1d2b409113481e369b691315
Sergei Omelchenko <ser.ommm@gmail.com>
2025-09-26 20:47:30 +0300

    Знакомство с faker-2

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,18 +7,12 @@ from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
+
 
 public_users_client = get_public_users_client()
 
 # Создаем пользователя
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
-)
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
 # Инициализируем клиенты
@@ -30,21 +24,12 @@ files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 
 # Загружаем файл
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="./testdata/files/cat.png"
-)
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/cat.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
 # Создаем курс
 create_course_request = CreateCourseRequestSchema(
-    title="Linux",
-    maxScore=10,
-    minScore=1,
-    description="Linux course",
-    estimatedTime="2 weeks",
     previewFileId=create_file_response.file.id,
     createdByUserId=create_user_response.user.id
 )
@@ -52,15 +37,7 @@ create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
 # Создаем задание
-create_exercise_request = CreateExerciseQuerySchema(
-    title='Start learning',
-    courseId=create_course_response.course.id,
-    maxScore=10,
-    minScore=1,
-    orderIndex=1,
-    description='Welcome onboard',
-    estimatedTime='2 hour'
-)
+create_exercise_request = CreateExerciseQuerySchema()
 exercise_client= get_exercise_client(authentication_user)
 create_exercise_response = exercise_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
