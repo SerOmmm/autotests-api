1dc414826f953d81055b086bd897bc1a79859099
Sergei Omelchenko <ser.ommm@gmail.com>
2025-10-10 18:30:28 +0300

    Перезапуски автотестов в Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,13 +9,19 @@ from fixtures.users import UserFixture  # Заменяем импорт
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
+from tools.fakers import fake
 
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
+@pytest.mark.parametrize("email", [
+    fake.email(domain="mail.ru"),
+    fake.email(domain="gmail.com"),
+    fake.email(domain="example.com")
+])
+def test_create_user(email: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
     # Удалили инициализацию API клиента из теста
-    request = CreateUserRequestSchema()
+    request = CreateUserRequestSchema(email=email)
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
